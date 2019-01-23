8c40dd4b33183246bca30b029f536d4e42032f1a
HiedanoAkyuu <ctzguozi@163.com>
2019-01-23 22:06:04 +0800

    20190123

--- USST/25/D.cpp
+++ USST/25/D.cpp
@@ -1,34 +1,79 @@
 #include <cstdio>
+#include <queue>
 #include <memory.h>
 
-typedef long long ll;
-ll dis[1005][1005],t,s,d,ans=0x3f3f3f3f3f3f3f3fll,tt=1;
+using namespace std;
 
-inline ll min(ll a,ll b){return a<b?a:b;}
-inline ll max(ll a,ll b){return a>b?a:b;}
+const int N=1e5+5;
+int dis[N],head[N],tot=0,t,s,d,ans=0x3f3f3f3f,vis[N];
+struct node
+{
+    int to,v,nxt;
+}e[N<<1];
+struct dij
+{
+    int num,v;
+    bool operator < (const dij &b) const {return v>b.v;}
+};
+priority_queue<dij> q;
+
+inline int min(int a,int b){return a<b?a:b;}
+
+inline void addedge(int u,int v,int w)
+{
+    e[tot].nxt=head[u];
+    e[tot].v=w;
+    e[tot].to=v;
+    head[u]=tot;
+    tot++;
+}
+
+void dijs()
+{
+    memset(vis,0,sizeof(vis));
+    memset(dis,0x3f,sizeof(dis));
+    dis[0]=0;
+    q.push((dij){0,0});
+    while(!q.empty())
+    {
+        dij p=q.top();
+        q.pop();
+        if(vis[p.num]>1) continue;
+        vis[p.num]++;
+        for(int i=head[p.num];~i;i=e[i].nxt)
+            if(dis[e[i].to]>dis[p.num]+e[i].v)
+            {
+                dis[e[i].to]=dis[p.num]+e[i].v;
+                q.push((dij){e[i].to,dis[e[i].to]});
+            }
+    }
+}
 
 int main()
 {
-    memset(dis,0x3f,sizeof(dis));
-    scanf("%lld%lld%lld",&t,&s,&d);
-    for(int i=0;i<t;i++)
+    memset(head,-1,sizeof(head));
+    scanf("%d%d%d",&t,&s,&d);
+    while(t--)
     {
-        ll x,y,z;
-        scanf("%lld%lld%lld",&x,&y,&z);
-        dis[x][y]=dis[y][x]=min(dis[y][x],z);
-        tt=max(tt,x);
-        tt=max(tt,y);
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        addedge(x,y,z);
+        addedge(y,x,z);
     }
-    for(int k=1;k<=tt;k++)
-        for(int i=1;i<=tt;i++)
-            for(int j=1;j<=tt;j++)
-                dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
-    for(int i=0;i<d;i++)
+    while(s--)
     {
-        ll x;
-        scanf("%lld",&x);
-        ans=min(ans,dis[1][x]);
+        int x;
+        scanf("%d",&x);
+        addedge(0,x,0);
+        addedge(x,0,0);
     }
-    printf("%lld",ans);
+    dijs();
+    while(d--)
+    {
+        int x;
+        scanf("%d",&x);
+        ans=min(ans,dis[x]);
+    }
+    printf("%d",ans);
     return 0;
 }
